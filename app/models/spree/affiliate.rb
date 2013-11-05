061fb20ee0ea077c35c66b34a7d72c7cf62ee59a
ZackYang <zackyang214@gmail.com>
2013-11-05 14:51:16 +0800

    Re #744 Add a uniq validation

--- app/models/spree/affiliate.rb
+++ app/models/spree/affiliate.rb
@@ -4,6 +4,8 @@ class Spree::Affiliate < ActiveRecord::Base
   belongs_to :partner, :class_name => "Spree::User", :foreign_key => :partner_id
   belongs_to :user, :class_name => 'Spree::User'
   has_many :events, :class_name => "Spree::AffiliateEvent"
+  
+  validates :user_id, uniqueness: true
 
   def name
     I18n.t(:affiliate_program)
